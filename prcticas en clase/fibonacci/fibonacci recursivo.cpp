9f401b3242518168b84ab299fdad49129385ad66
HeidyMarin <heidy.marin@ucsp.edu.pe>
2017-04-07 06:40:27 -0500

    Add files via upload

---
+++ prcticas en clase/fibonacci/fibonacci recursivo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+	if(n<=1)
+		return n;
+	return fib(n-1) + fib(n-2);
+}
+
+int main()
+{
+	int n;
+	cout<< "     FIBONACCI     "<< endl;
+	cout<< "\nIngrese el valor a calcular: ";
+	cin>>n;
+	cout<<"\n Resultado: ";
+	cout << fib(n);
+	return 0;
+}
+
---
+++ prcticas en clase/fibonacci/fibonacci.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+	if(n<=1)
+		return n;
+	
+	int result, a=0, b=1, i;
+	for(i=2; i<=n; i++)
+	{
+		result = a + b;
+		a = b;
+		b = result;
+	}
+	return result;
+}
+
+int main()
+{
+	int n;
+	cout<< "      FIBONACCI     "<< endl;
+	cout<< "\nIngrese el valor a calcular: ";
+	cin>>n;
+	cout<<"\n Resultado: ";
+	cout << fib(n);
+	return 0;
+}
+
