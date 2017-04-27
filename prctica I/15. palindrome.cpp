44a61e8324e5fbe0cb2311f9509d4c607c1dbabf
HeidyMarin <heidy.marin@ucsp.edu.pe>
2017-04-27 00:54:44 -0500

    Add files via upload

---
+++ prctica I/15. palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool palindrome(char a[], int n)
+{
+	for(int i=0; i<n/2; i++){
+		if(a[i] != a[n-1-i])
+			return false;
+	}
+	return true;
+}
+
+int main() 
+{
+	char palabra[] = "holaloh";
+	int n = 7;
+	palindrome(palabra, n)? cout<<" true " : cout<<" false ";
+//	if(palindrome(palabra, n) == 1)
+//		cout<< " true "<<endl;
+//	else
+//		cout<< " false ";
+	return 0;
+}
+
